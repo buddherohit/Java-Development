ee743b7fe5b29ad62c9c03408d219c4766ee3dcc
buddherohit <rohitbudddhe564@gmail.com>
2026-02-22 13:50:17 +0530

    fresh start

--- Java Developemnt/Day-12/CgpaCalculation.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class CgpaCalculation{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter total subjects");
-    }
-    
-}
--- Java Developemnt/Day-15/multiplicationTable.java
+++
@@ -1,9 +0,0 @@
-public class multiplicationTable {
-    public static void main(String args[]){
-        int n=5;
-        for(int i=10;i>0;i--){
-            System.out.println(n+" x "+i+" = "+(n*i));
-        }
-    }
-    
-}
